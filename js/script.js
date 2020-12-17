2d8bdca568fff7d6f8a3e7c1f12b11bfdba132f1
inshin <inshin@student.42seoul.kr>
2020-12-18 04:13:31 +0900

    fix js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
 $(document).ready(function(){
-	var newName = "abc"
-	var name = $(".man_1 .man_name");
-	name.text(newName);
+	// var newName = "abc"
+	// var name = $(".man_1 .man_name");
+	// name.text(newName);
 
 	// 첫화면
 	$(".blackhole").click(function(){
 		$(".modal").slideUp();
 		$(this).css({
-			"transform": "translate(-50%, 50%)",
-			"transition": "1s"
+            "transform": "translate(-50%, 50%)",
+            "transition": "1s"
 		})
+		$(this).addClass("blurEffect")
 		$("#Layer_1").css({
 			"animation": "bkhole_ani 10s infinite linear"
 		});
 		$("canvas").show();
 		$(".main").show(1500);
 	});
-
-	$('#frame1').load(function(){
-		var $iframe = $('#frame1').contents();
-		$iframe.find('text');
+	$(".man").click(function(){
+		$(this).find(".man_talk").toggle();
 	});
 });
